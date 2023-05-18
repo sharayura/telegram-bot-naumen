32214e7006fa8a990a5a63c5c05f972378a02395
sharayura <sharapyura@gmail.com>
2023-05-18 22:22:20 +0300

    Initial commit

---
+++ src/main/java/ru/yuri/telegrambotnaumen/TelegramBotNaumenApplication.java
@@ -0,0 +1,13 @@
+package ru.yuri.telegrambotnaumen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotNaumenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotNaumenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yuri/telegrambotnaumen/TelegramBotNaumenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yuri.telegrambotnaumen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotNaumenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
